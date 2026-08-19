018484838c265c8a456a50cee770b44d15141c0b
William <wliem5741@gmail.com>
2026-08-19 16:43:49 +0000

    changed greeting message

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,6 +1,6 @@
 public class Greeter {
     public String greet(String name) {
         // You can change the greeting message if you want
-        return "Good day, " + name;
+        return "howdy, " + name;
     }
 }
